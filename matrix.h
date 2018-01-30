64eb7be76bb8bf697b0ab631c008057a361fb27d
Victor Coda <magma.github@gmail.com>
2018-01-31 00:22:24 +0200

    Implemented matrix and vector multiplication operators

--- matrix.h
+++ matrix.h
@@ -7,6 +7,11 @@ namespace rapid
         matrix(const XMMATRIX& m): XMMATRIX(m) {}
         matrix(const class quaternion& q);
 
+        matrix operator*(const matrix& m) const { return XMMatrixMultiply(*this, m); }
+        vector2 operator*(const vector2& v) const { return XMVector2Transform(v.V, *this); }
+        vector3 operator*(const vector3& v) const { return XMVector3Transform(v.V, *this); }
+        vector4 operator*(const vector4& v) const { return XMVector4Transform(v.V, *this); }
+
         bool nan() const { return XMMatrixIsNaN(*this); }
         bool infinite() const { return XMMatrixIsInfinite(*this); }
         bool unit() const { return XMMatrixIsIdentity(*this); }
