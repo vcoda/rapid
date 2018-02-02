c4bb0df56ae68f68b78b1d281c362152b51763ac
Victor Coda <magma.github@gmail.com>
2018-02-02 15:06:23 +0200

    Implemented template min/max functions

--- rapid.h
+++ rapid.h
@@ -30,4 +30,7 @@ namespace rapid
 {
     inline float radians(const float degrees) { return XMConvertToRadians(degrees); }
     inline float degrees(const float radians) { return XMConvertToDegrees(radians); }
+
+    template<typename T> inline T min(const T& a, const T& b) { return a < b ? a : b; }
+    template<typename T> inline T max(const T& a, const T& b) { return a > b ? a : b; }
 } // namespace rapid
