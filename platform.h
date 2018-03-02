b4c4624612f34c9bb392957b0992d04583d648b2
Victor Coda <magma.github@gmail.com>
2018-03-02 04:28:23 -0800

    Check that attribute macro wasn't already defined before defining it. Some attributes couldn't be ported properly due to order issue.

--- platform.h
+++ platform.h
@@ -1,21 +1,36 @@
 #pragma once
 #if !defined(_MSC_VER)
+
+#ifndef align
+#define align(size) // aligned(size) // warning: attribute ignored
+#endif
+
+#ifndef selectany
+#define selectany weak
+#endif
+
+#ifndef __declspec
+#define __declspec(modifier) __attribute__((modifier))
+#define _declspec(modifier) __declspec(modifier)
+#endif
+
+#ifndef __fastcall
+#define __fastcall // fastcall // warning: attribute ignored
+#endif
+
 #include <cpuid.h>
 
 #ifdef __cpuid
 #undef __cpuid
 #endif
-
 #define __cpuid(cpu, func)\
 	__cpuid_count(func, 0, cpu[0], cpu[1], cpu[2], cpu[3])
+
+#ifdef __cpuidex
+#undef __cpuidex
+#endif
+// TODO: subfunc
 #define __cpuidex(cpu, func, subfunc)\
 	__cpuid_count(func, 0, cpu[0], cpu[1], cpu[2], cpu[3])
-
-#define _declspec(modifier)
-#define __declspec(modifier)
-#define __fastcall
-
-#define align(alignment)
-#define selectany
 
 #endif // !_MSC_VER
