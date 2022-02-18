3e4f3ffa07b917a97dcb36d5ee9d225f358db531
Victor Coda <victor.coda@gmail.com>
2022-02-18 15:42:26 +0200

    Use internal union vec4

--- cosinelut.h
+++ cosinelut.h
@@ -5,15 +5,26 @@ namespace rapid
     public:
         explicit cosinelut(float start, float step, uint32_t count);
         ~cosinelut() { delete[] lut; }
-        float sin(uint32_t i) const noexcept { return lut[i/4].vsin.f[i%4]; }
-        float cos(uint32_t i) const noexcept { return lut[i/4].vcos.f[i%4]; }
+        float sin(uint32_t i) const noexcept { return lut[i/4].vsin.a[i%4]; }
+        float cos(uint32_t i) const noexcept { return lut[i/4].vcos.a[i%4]; }
 
     private:
         const uint32_t size;
+
+        union vec4
+        {
+            float a[4];
+        #if defined(__x86_64__) || defined(_M_AMD64)
+            float4a v;
+        #else
+            float4 v;
+        #endif
+        };
+
         struct vsincos
         {
-            flint4 vsin;
-            flint4 vcos;
+            vec4 vsin;
+            vec4 vcos;
         } *lut;
     };
 } // namespace rapid
